2794eb0c9d194ea8e9234ec0d10a474afed3c906
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-11-18 10:39:54 -0600

    Create file retriever class
    This commit creates the file retriever class and implements it.
    
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -1,25 +1,39 @@
 package segmentedfilesystem;
-
+import java.io.IOException;
+import java.net.*;
 public class FileRetriever {
+	private InetAddress server;
+	private int port;
 
 	public FileRetriever(String server, int port) {
-        // Save the server and port for use in `downloadFiles()`
-        //...
-	}
+                this.port = port;
 
-	public void downloadFiles() {
-        // Do all the heavy lifting here.
-        // This should
-        //   * Connect to the server
-        //   * Download packets in some sort of loop
-        //   * Handle the packets as they come in by, e.g.,
-        //     handing them to some PacketManager class
-        // Your loop will need to be able to ask someone
-        // if you've received all the packets, and can thus
-        // terminate. You might have a method like
-        // PacketManager.allPacketsReceived() that you could
-        // call for that, but there are a bunch of possible
-        // ways.
-	}
+                try {
+                        this.server = InetAddress.getByName(server);
+                } catch(UnknownHostException e) {
+                        e.printStackTrace();
+                }
+        }
 
-}
+	public void downloadFiles() throws IOException {
+		PacketManager packetManager = new PacketManager();
+		DatagramSocket socket = new DatagramSocket();
+
+		try {
+			byte[] buf = new byte[1028];
+			DatagramPacket packet = new DatagramPacket(buf, buf.length, server, port);
+			socket.send(packet);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	
+		while(!packetManager.haveAllFiles()){
+			byte[] data = new byte[1028];
+			DatagramPacket packet = new DatagramPacket(data, 1028);
+			socket.receive(packet);
+			packetManager.addPacket(packet);
+		}
+		packetManager.writeAllFiles();
+	}
+}	
