633b30f338f613297cf5c58d6e2f21afc9b0c883
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-11-18 11:12:12 -0600

    Add comments to FileRetriever
    This commit adds comments to the FileRetriever class for clarity purposes.
    
    Co-authored-by: Danter Miller <mill8622@morris.umn.edu>

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -4,11 +4,14 @@ import java.net.*;
 public class FileRetriever {
 	private InetAddress server;
 	private int port;
-
+	
+	//Constructor for FileRetriever
 	public FileRetriever(String server, int port) {
+		//Save port number
                 this.port = port;
 
                 try {
+			//Get IP address associated with the server name
                         this.server = InetAddress.getByName(server);
                 } catch(UnknownHostException e) {
                         e.printStackTrace();
@@ -16,24 +19,32 @@ public class FileRetriever {
         }
 
 	public void downloadFiles() throws IOException {
+		//Create instance of packetManager
 		PacketManager packetManager = new PacketManager();
+		// Create socket
 		DatagramSocket socket = new DatagramSocket();
 
 		try {
+			//Create byte buffer
 			byte[] buf = new byte[1028];
+			//Create empty datagram packet to send to server
 			DatagramPacket packet = new DatagramPacket(buf, buf.length, server, port);
+
+			//Send empty packet so sever
+			//This tells the server to start sending files to us
 			socket.send(packet);
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
 		
-	
-		while(!packetManager.haveAllFiles()){
+		//While loop to keep receiving packets from the server until we have received all the files
+		while(!packetManager.haveAllFiles()){	
 			byte[] data = new byte[1028];
 			DatagramPacket packet = new DatagramPacket(data, 1028);
 			socket.receive(packet);
 			packetManager.addPacket(packet);
 		}
+		//Once while loop has completed, write all the files to disk
 		packetManager.writeAllFiles();
 	}
 }	
